71fedc4d4af25b61cb095c22deeaf3e5e1fe64e4
Nick <nicholasg1989@gmail.com>
2020-07-28 17:39:14 -0700

    comments

--- validate.py
+++ validate.py
@@ -19,18 +19,33 @@ def verify_google_jwt(token,
                       audience=None,
                       certs={},
                       return_certs=False):
+                    
+    # Get the token header so we know which cert it was signed with (kid)
     header = jwt.get_unverified_header(token)
     kid = header['kid']
+
+    # If the token kid is not in the provided certs dict then call out to google to get an updated list of certs
     if kid not in certs:
         certs = _get_certs(certs_url)
-        cert = certs[kid].encode('ascii')
-        public_key = _get_public_key(cert)
+
+    # Get the cert specified in our token header as bytes
+    # If a matching kid was not provided and it not found online, will raise KeyError
+    cert = certs[kid].encode('ascii')
+
+    # Turn the cert into a public key so the pyjwt can consume it
+    # For some reason this is more complicated than passing it a string
+    public_key = _get_public_key(cert)
     
+    # Get the decoded token if the signature is legit
+    # A forged token will raise jwt.exceptions.InvalidSignatureError
     decoded = jwt.decode(token, public_key, algorithms=['RS256'], audience=audience)
 
     if return_certs:
+        # return the decoded token and the used cert list for caching
         return decoded, certs
+
     else:
+        # Just return the decoded token
         return decoded
     
 if __name__ == '__main__':
@@ -40,7 +55,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         exit('Usage: {0} <audience/aud> <token>'.format(*sys.argv))
 
-    # audience or "aud"
+    # audience or "aud". This should be your firebase project name.
     audience = sys.argv[1]
 
     # the json token returned from firebase auth
